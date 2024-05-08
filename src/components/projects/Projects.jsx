ecc078a48b4596bd6aa815ddced1bb78b4bdb240
peggyp <proffit.peggy@yahoo.fr>
2024-05-08 22:53:36 +0200

    creating Projects component and css

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import projectsData from '../data/projects.json';
 
-const Projects = () => {
+const Projects = () => {   
+   
+
     return (
         <div className='container_Projects'>
             <h2>Mes projets</h2>
-            
+            {projectsData.map((project) => (        
+                <div key={project.id} className='projects'>
+                    <div className='container_image'>
+                        <img src={project.image} alt={project.title} />
+                    </div>
+                    <div className="container_content">
+                        <p className='title'>{project.title}</p>
+                        <p className='description'><span className='objectif'>Objectif :</span><br /> {project.description}</p>
+                    </div>
+                    <div className='container_Links_Projects'>
+                        <a href={project.Link_Github} target="_blank"><p className='link_Github' >Lien Github</p></a>
+                        
+                        
+                    </div>
+                
+                </div>      
+            ))}  
         </div>
     );
 };
