3758eb2f23cba44e0e9fdd4a4aa5015f725c2dad
peggyp <proffit.peggy@yahoo.fr>
2024-05-08 11:39:30 +0200

    creating Skills component and file css and file json

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import skillsData from '../data/skills.json';
 
-const Skills = () => {
+const Skills = ({}) => {
+
     return (
-        <div>
-            
-        </div>
+        <section className='container_Skills'>
+            {skillsData.map(skillsData => (
+                <div key={skillsData.id} className='container_Img_and_NameLogo' >
+                    <img src={skillsData.image} alt={skillsData.nameLogo}/>
+                    <p>{skillsData.nameLogo}</p>
+                </div>
+            ))}
+        </section>    
     );
 };
 
