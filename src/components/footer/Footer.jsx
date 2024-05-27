71e6411df6e0dad13d68af067bd80c056ebfbadd
peggyp <proffit.peggy@yahoo.fr>
2024-05-27 20:41:14 +0200

    Adding target=_blank

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
         <div className='footer-Container'>
             <div className='social-Icons'>
                 <a href="https://github.com/PeggyPft">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_Github.png" alt="Logo github" />
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_Github.png" target="_blank" alt="Logo github" />
                 </a>
                 <a href="https://www.linkedin.com/in/peggy-proffit">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_Linkedin.png" alt="Logo github" />
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_Linkedin.png" target="_blank" alt="Logo github" />
                 </a>
                 <a href="mailto:proffit.peggy@yahoo.fr">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_mail.svg" alt="Logo github" />
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_mail.svg" target="_blank" alt="Logo github" />
                 </a>
             </div>
         </div>
