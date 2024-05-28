c2997fdf79e11f8dfe5b7cce6248680fc7116763
peggyp <proffit.peggy@yahoo.fr>
2024-05-28 20:07:15 +0200

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
     return (
         <div className='footer-Container'>
             <div className='social-Icons'>
-                <a href="https://github.com/PeggyPft">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_Github.png" target="_blank" alt="Logo github" />
+                <a href="https://github.com/PeggyPft" target="_blank">
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_Github.png" alt="Logo github" />
                 </a>
-                <a href="https://www.linkedin.com/in/peggy-proffit">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_Linkedin.png" target="_blank" alt="Logo github" />
+                <a href="https://www.linkedin.com/in/peggy-proffit" target="_blank">
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_Linkedin.png" alt="Logo github" />
                 </a>
-                <a href="mailto:proffit.peggy@yahoo.fr">
-                    <img src="https://peggypft.github.io/Portfolio/images/logo_mail.svg" target="_blank" alt="Logo github" />
+                <a href="mailto:proffit.peggy@yahoo.fr" target="_blank">
+                    <img src="https://peggypft.github.io/Portfolio/images/logo_mail.svg" alt="Logo github" />
                 </a>
             </div>
         </div>
